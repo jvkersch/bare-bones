9fa3f60f5de2e6481bbefa96619e24e953ac79bf
Stefano Borini <sborini@enthought.com>
2016-12-06 15:29:29 +0000

    Fixes setup.py to work with recent version of setuptools

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 #! /usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 with open('README.md') as f:
     long_description = f.read()
 
 setup(name='bare-bones',
-      version='0.0',
+      version='0.1',
       author='Enthought Inc.',
       description='A skeleton Tasks/Envisage application',
       long_description=long_description,
       author_email='xxx@enthought.com',
       url='',  # TODO Update this
-      requires=['envisage'],
-      py_modules=['bare_bones'],
+      install_requires=[
+        'envisage~=4.5',
+        'traits~=4.6'
+      ],
+      packages=find_packages(),
+      package_data={
+          'bare_bones': ['preferences.ini'],
+      },
+      zip_safe=False,
       entry_points={
           'console_scripts': [
               'bare_bones_app = bare_bones.run:main',
